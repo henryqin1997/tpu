41021d80ef7c361ea1a6cff484feef982c812298
henryqin <henryqin@umich.edu>
2021-01-10 23:04:10 -0800

    shell

--- models/official/resnet/run.sh
+++ models/official/resnet/run.sh
@@ -2,4 +2,4 @@ export TPU_NAME=ziheng
 export DATA_DIR=gs://imagenet2012/imagenet-2012-tfrecord/
 export MODEL_DIR=gs://ziheng_store
 gsutil -m rm -R -f $MODEL_DIR/*
-python official/resnet/resnet_main_batch_step.py —tpu=ziheng —log_step_count_steps 1 —resnet_depth 50 --train_batch_size 1024 —mode train —train_steps 2502 --data_dir=$DATA_DIR --model_dir=$MODEL_DIR
+python3 official/resnet/resnet_main_batch_step.py —tpu=ziheng —log_step_count_steps 1 —resnet_depth 50 --train_batch_size 1024 —mode train —train_steps 2502 --data_dir=$DATA_DIR --model_dir=$MODEL_DIR
