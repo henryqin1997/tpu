2d34022d12589dbaf0615c10ddf8a0dcf1228fd7
henryqin <henryqin@umich.edu>
2021-01-10 16:11:43 -0800

    code for lrrt

---
+++ models/official/resnet/onecycle.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+
+class CosineAnnealer:
+
+    def __init__(self, start, end, steps):
+        self.start = start
+        self.end = end
+        self.steps = steps
+        self.n = 0
+
+    def step(self):
+        self.n += 1
+        cos = np.cos(np.pi * (self.n / self.steps)) + 1
+        return self.end + (self.start - self.end) / 2. * cos
+
+    def getval(self,step):
+        cos = np.cos(np.pi * (step / self.steps)) + 1
+        return self.end + (self.start - self.end) / 2. * cos
+
+class OneCycleScheduler():
+    """
+    From https://www.avanwyk.com/tensorflow-2-super-convergence-with-the-1cycle-policy/, by Andrich van Wyk modified from
+    fastai lib. Modified again to apply for TPU code.
+    """
+
+    def __init__(self, lr_max, steps, mom_min=0.85, mom_max=0.95, phase_1_pct=0.3, div_factor=25.):
+        super(OneCycleScheduler, self).__init__()
+        lr_min = lr_max / div_factor
+        final_lr = lr_max / (div_factor * 1e4)
+        phase_1_steps = steps * phase_1_pct
+        phase_2_steps = steps - phase_1_steps
+
+        self.phase_1_steps = phase_1_steps
+        self.phase_2_steps = phase_2_steps
+        self.phase = 0
+        self.step = 0
+
+        self.phases = [[CosineAnnealer(lr_min, lr_max, phase_1_steps), CosineAnnealer(mom_max, mom_min, phase_1_steps)],
+                       [CosineAnnealer(lr_max, final_lr, phase_2_steps),
+                        CosineAnnealer(mom_min, mom_max, phase_2_steps)]]
+
+        self.lrs = []
+        self.moms = []
+
+    def getlrmom(self,step):
+        if step<self.phase_1_steps:
+            return self.phases[0][0].getval(step),self.phases[0][1].getval(step)
+        else:
+            return self.phases[1][0].getval(step-self.phase_1_steps),self.phases[1][1].getval(step-self.phase_1_steps)
+
+
+
+
+def lrs(step,total_step):
+    low = math.log2(1e-5)
+    high = math.log2(10)
+    return 2**(low+(high-low)*step/total_step)
