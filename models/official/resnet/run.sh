57ab04b1a4d3f403191f904c3bee6d7e4981a252
henryqin <henryqin@umich.edu>
2021-01-11 15:16:53 -0800

    new range test

--- models/official/resnet/run.sh
+++ models/official/resnet/run.sh
@@ -4,4 +4,4 @@ export MODEL_DIR=gs://ziheng_store
 export DATA_DIR=gs://imagenet2012/imagenet-2012-tfrecord/
 #export DATA_DIR=gs://cloud-tpu-test-datasets/fake_imagenet
 gsutil -m rm -R -f $MODEL_DIR/*
-python3 official/resnet/resnet_main_batch_step.py —-tpu=ziheng --resnet_depth=50 --train_steps=1024 —-mode=train —-train_steps=2502 --data_dir=$DATA_DIR --model_dir=$MODEL_DIR
+python3 official/resnet/resnet_main_batch_step.py —-tpu=ziheng --resnet_depth=50 --train_steps=200 —-mode=train --data_dir=$DATA_DIR --model_dir=$MODEL_DIR
