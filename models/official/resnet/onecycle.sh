b011907a4936aa3396179677a8a5b88d5585c2ec
henryqin <henryqin@umich.edu>
2021-01-11 18:48:21 -0800

    updata

--- models/official/resnet/onecycle.sh
+++ models/official/resnet/onecycle.sh
@@ -3,4 +3,5 @@ export TPU_NAME=ziheng
 export MODEL_DIR=gs://ziheng_store
 export DATA_DIR=gs://imagenet2012/imagenet-2012-tfrecord/
 gsutil -m rm -R -f $MODEL_DIR/*
-python3 official/resnet/resnet_main_onecycle.py —-tpu=ziheng --resnet_depth=50 --data_dir=$DATA_DIR --model_dir=$MODEL_DIR --train_steps=200 --mode=train
+python3 official/resnet/resnet_main_onecycle.py —-tpu=ziheng --resnet_depth=50 --data_dir=$DATA_DIR --model_dir=$MODEL_DIR
+#--train_steps=200 --mode=train
