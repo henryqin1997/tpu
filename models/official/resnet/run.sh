cacf2e396ff98d1da5e942785032baf40d54ab64
henryqin <henryqin@umich.edu>
2021-01-12 20:51:27 -0800

    updata

--- models/official/resnet/run.sh
+++ models/official/resnet/run.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH="$PYTHONPATH:/path/to/models"
 export TPU_NAME=ziheng
-export MODEL_DIR=gs://ziheng_store/larslrrt/
+export MODEL_DIR=gs://ziheng_store/
 export DATA_DIR=gs://imagenet2012/imagenet-2012-tfrecord/
 export ACCELERATOR_TYPE=v3-1024
 gsutil -m rm -R -f $MODEL_DIR/*
@@ -10,5 +10,5 @@ python3 official/resnet/resnet_main_batch_step.py --tpu=${TPU_NAME} \
    --train_steps=200 --mode=train --config_file=official/resnet/configs/cloud/${ACCELERATOR_TYPE}.yaml
 python3 official/resnet/resnet_main_batch_step.py —-tpu=ziheng \
  --enable_lars --resnet_depth=50 --data_dir=$DATA_DIR \
-  --model_dir=$MODEL_DIR --train_batch_size=32768 --train_steps=200 \
+  --model_dir=gs://ziheng_store/lars/ --train_batch_size=32768 --train_steps=200 \
    --mode=train --config_file=official/resnet/configs/cloud/${ACCELERATOR_TYPE}.yaml
